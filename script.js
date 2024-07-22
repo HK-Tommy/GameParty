a43e8754f7c2541e1201ecd59841a78f55aa8c98
HK 湯米Tommy <tlamhappy10@gmail.com>
2024-07-22 17:56:09 +0800

    Update

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 function redirect(url) {
     window.open(url, '_blank');
 }
+setTimeout(function() {
+    document.getElementById("loader").style.display = 'none';
+    document.getElementById('game-container').style.display = '';
+    document.getElementById('footer').style.display = ''
+}, 2000);
